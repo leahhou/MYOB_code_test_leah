0b9854fc7823530d578d32fbdeab8d817571b4ee
leahhou <hsx_124@hotmail.com>
2019-07-17 12:35:54 +1000

    update README file

--- play/valid_input.rb
+++ play/valid_input.rb
@@ -3,11 +3,11 @@ require 'colorize'
 class ValidInput
     def instruct_input(coord_input,coord)
         inputVerify = [0,1,2]
-        puts  "place " + "#{coord}".colorize(:yellow) + "-coord" + " (please enter number 1,2,3 only)".colorize(:yellow)
+        puts  "place " + "#{coord}".colorize(:yellow) + "-coord" + " (please enter number 1,2,3 only)".colorize(:yellow) + " and press enter"
         coord_input = gets.chomp
         until coord_input.length == 1
             wrong_input
-            puts  "place " + "#{coord}".colorize(:yellow) + "-coord" + " (please enter number 1,2,3 only)".colorize(:yellow)
+            puts  "place " + "#{coord}".colorize(:yellow) + "-coord" + " (please enter number 1,2,3 only)".colorize(:yellow) + " and press enter"
             coord_input = gets.chomp
             coord_input
             # i += 0
@@ -15,7 +15,7 @@ class ValidInput
         coord_input = coord_input.to_i - 1
         until inputVerify.include?(coord_input)
             wrong_input
-            puts  "place " + "#{coord}".colorize(:yellow) + "-coord" + " (please enter number 1,2,3 only)".colorize(:yellow)
+            puts  "place " + "#{coord}".colorize(:yellow) + "-coord" + " (please enter number 1,2,3 only)".colorize(:yellow) + " and press enter"
             coord_input = gets.chomp.to_i - 1
             coord_input
             # i += 0
